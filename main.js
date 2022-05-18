af31121795463e2da4b012becc06edf6e9ce2fd0
jbryans <jbryans2014@gmail.com>
2022-05-17 20:12:25 -0500

    function

--- main.js
+++ main.js
@@ -9,10 +9,35 @@ const rl = readline.createInterface({
 
 
 
+
+console.clear()
 const countIt = (str) => {
-  console.log('BOOMS', str.length)
+  let cleanString = str.toLowerCase().replace(/[^A-Za-z0-9]/g, '')
+  console.log(cleanString)
+  const counts = {};
+  for (const i of cleanString) {
+    if (counts[i]) {
+      counts[i]++
+    } else {
+      counts[i] = 1
+    }
+  }
+  return counts;
 }
 
+// const string = "I want to count the number of occurences of each char in this string";
+// console.log(countChar(string))
+
+// const obj = {
+//   name: 1,
+//   address: "San Marcos"
+// }
+
+// console.log(obj)
+
+// obj.name = 'string'
+// obj.address = 1
+// obj.address++
 
 const getPrompt = () =>  {
   rl.question('Enter your string to count the characters: ', (str) => {
@@ -20,6 +45,20 @@ const getPrompt = () =>  {
     getPrompt();
   });
 }
+
+//take any string 
+
+//remove whitespace/convert to lower case
+//toLowerCase()/.trim()
+//.split to show each letter in  an array indiividually
+//the output shows the letters individually and how often they appear in the string
+
+
+//**********************************
+//**********************************
+
+
+
 
 if (typeof describe === 'function') {
   describe('#countIt()', () => {
